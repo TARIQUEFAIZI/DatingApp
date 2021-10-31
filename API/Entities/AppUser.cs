eb38b1585c9fea10b6c1b226cfecd4f2e849e0cf
Tarique Faizi <faizi.tarique@gmail.com>
2021-11-01 00:00:44 +0530

    EndofS2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        [Column(TypeName ="varchar(50)")]
+        public string UserName { get; set; }
+        
+    }
+}
